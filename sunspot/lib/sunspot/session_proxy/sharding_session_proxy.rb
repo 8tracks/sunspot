e89eb2771eb3aedfbc8569a3876cdaa32165cdad
Netto Farah <nettofarah@gmail.com>
2013-12-18 14:21:02 -0500

    fixes ClassShardingSessionProxy bug (wont pass in the block with search params)

--- sunspot/lib/sunspot/session_proxy/sharding_session_proxy.rb
+++ sunspot/lib/sunspot/session_proxy/sharding_session_proxy.rb
@@ -2,7 +2,7 @@ require File.join(File.dirname(__FILE__), 'abstract_session_proxy')
 
 module Sunspot
   module SessionProxy
-    # 
+    #
     # This is a generic abstract implementation of a session proxy that allows
     # Sunspot to be used with a distributed (sharded) Solr deployment. Concrete
     # subclasses should implement the #session_for method, which takes a
@@ -31,14 +31,14 @@ module Sunspot
     class ShardingSessionProxy < AbstractSessionProxy
       not_supported :batch, :config, :remove_by_id, :remove_by_id!
 
-      # 
+      #
       # +search_session+ is the session that should be used for searching.
       #
       def initialize(search_session = Sunspot.session.new)
         @search_session = search_session
       end
 
-      # 
+      #
       # Return the appropriate shard session for the object.
       #
       # <strong>Concrete subclasses must implement this method.</strong>
@@ -47,7 +47,7 @@ module Sunspot
         raise NotImplementedError
       end
 
-      # 
+      #
       # Return all shard sessions.
       #
       # <strong>Concrete subclasses must implement this method.</strong>
@@ -56,35 +56,35 @@ module Sunspot
         raise NotImplementedError
       end
 
-      # 
+      #
       # See Sunspot.index
       #
       def index(*objects)
         using_sharded_session(objects) { |session, group| session.index(group) }
       end
 
-      # 
+      #
       # See Sunspot.index!
       #
       def index!(*objects)
         using_sharded_session(objects) { |session, group| session.index!(group) }
       end
 
-      # 
+      #
       # See Sunspot.remove
       #
       def remove(*objects)
         using_sharded_session(objects) { |session, group| session.remove(group) }
       end
 
-      # 
+      #
       # See Sunspot.remove!
       #
       def remove!(*objects)
         using_sharded_session(objects) { |session, group| session.remove!(group) }
       end
 
-      # 
+      #
       # If no argument is passed, behaves like Sunspot.remove_all
       #
       # If an argument is passed, will raise NotSupportedError, as the proxy
@@ -98,7 +98,7 @@ module Sunspot
         end
       end
 
-      # 
+      #
       # If no argument is passed, behaves like Sunspot.remove_all!
       #
       # If an argument is passed, will raise NotSupportedError, as the proxy
@@ -112,21 +112,21 @@ module Sunspot
         end
       end
 
-      # 
+      #
       # Commit all shards. See Sunspot.commit
       #
       def commit
         all_sessions.each { |session| session.commit }
       end
 
-      # 
+      #
       # Optimize all shards. See Sunspot.optimize
       #
       def optimize
         all_sessions.each { |session| session.optimize }
       end
 
-      # 
+      #
       # Commit all dirty sessions. Only dirty sessions will be committed.
       #
       # See Sunspot.commit_if_dirty
@@ -135,17 +135,17 @@ module Sunspot
         all_sessions.each { |session| session.commit_if_dirty }
       end
 
-      # 
+      #
       # Commit all delete-dirty sessions. Only delete-dirty sessions will be
       # committed.
-      # 
+      #
       # See Sunspot.commit_if_delete_dirty
       #
       def commit_if_delete_dirty
         all_sessions.each { |session| session.commit_if_delete_dirty }
       end
 
-      # 
+      #
       # Instantiate a new Search object, but don't execute it. The search will
       # have an extra :shards param injected into the query, which will tell the
       # Solr instance referenced by the search session to search across all
@@ -163,7 +163,7 @@ module Sunspot
         search
       end
 
-      # 
+      #
       # Build and execute a new Search. The search will have an extra :shards
       # param injected into the query, which will tell the Solr instance
       # referenced by the search session to search across all shards.
@@ -171,7 +171,7 @@ module Sunspot
       # See Sunspot.search
       #
       def search(*types, &block)
-        new_search(*types).execute
+        new_search(*types, &block).execute
       end
 
       def more_like_this(object, &block)
@@ -183,7 +183,7 @@ module Sunspot
         @search_session.new_more_like_this(object, &block)
       end
 
-      # 
+      #
       # True if any shard session is dirty. Note that directly using the
       # #commit_if_dirty method is more efficient if that's what you're
       # trying to do, since in that case only the dirty sessions are committed.
@@ -194,7 +194,7 @@ module Sunspot
         all_sessions.any? { |session| session.dirty? }
       end
 
-      # 
+      #
       # True if any shard session is delete-dirty. Note that directly using the
       # #commit_if_delete_dirty method is more efficient if that's what you're
       # trying to do, since in that case only the delete-dirty sessions are
@@ -206,7 +206,7 @@ module Sunspot
 
       private
 
-      # 
+      #
       # Group the objects by which shard session they correspond to, and yield
       # each session and is corresponding group of objects.
       #
